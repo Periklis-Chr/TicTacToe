8e0ecf6451b42a520849f4878c86c0d800341659
thankarezos <thankarezos@gmail.com>
2021-11-26 18:00:08 +0200

    game innit

--- login_fxml/src/login_fxml/Login_fxml.java
+++ login_fxml/src/login_fxml/Login_fxml.java
@@ -21,7 +21,6 @@ public class Login_fxml extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
         stage.show();
     }
