851be1e3f4333c0b0338c2013904d769232333df
Periklis-Chr <it175073@it.teithe.gr>
2021-12-04 06:18:18 +0200

    one error passField equal

--- src/main/java/gr/ihu/tictactoe/InfoConnection.java
+++ src/main/java/gr/ihu/tictactoe/InfoConnection.java
@@ -1,25 +1,25 @@
-package gr.ihu.tictactoe;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-public class InfoConnection {
-    // Standar info connection
-    static String     driverClassName = "com.mysql.jdbc.Driver" ;
-    static String     url             = "jdbc:mysql://users.it.ihu.gr:5432/it175073" ;
-    static Connection dbConnection    = null;
-    static String     username        = "it175073";
-    static String     passwd          = "!Perisarrow23..";
-    static Statement  statement       = null;
-    static ResultSet  rs              = null;
-    //End
-
-
-}
+//package gr.ihu.tictactoe;
+//
+//import java.sql.Connection;
+//import java.sql.ResultSet;
+//import java.sql.Statement;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.scene.input.MouseEvent;
+//import javafx.stage.Stage;
+//import javafx.stage.StageStyle;
+//
+//public class InfoConnection {
+//    // Standar info connection
+//    static String     driverClassName = "com.mysql.jdbc.Driver" ;
+//    static String     url             = "jdbc:mysql://users.it.ihu.gr:5432/it175073" ;
+//    static Connection dbConnection    = null;
+//    static String     username        = "it175073";
+//    static String     passwd          = "";
+//    static Statement  statement       = null;
+//    static ResultSet  rs              = null;
+//    //End
+//
+//
+//}
